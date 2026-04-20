82f89019be32f64768a1b2bb9239a717d7c2a24d
dosesafe <hschoeman35@gmail.com>
2026-04-20 16:04:59 +0200

    Update database.py

--- database.py
+++ database.py
@@ -7,15 +7,26 @@ def create_tables():
     conn = connect()
     c = conn.cursor()
 
+    # CHILDREN
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS children (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT
+        )
+    """)
+
+    # MEDS (linked to child)
     c.execute("""
         CREATE TABLE IF NOT EXISTS meds (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            child_id INTEGER,
             name TEXT,
             dosage TEXT,
             interval INTEGER
         )
     """)
 
+    # LOGS
     c.execute("""
         CREATE TABLE IF NOT EXISTS logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,24 +39,50 @@ def create_tables():
     conn.close()
 
 
-def add_med(name, dosage, interval):
+# -------------------------
+# CHILD FUNCTIONS
+# -------------------------
+def add_child(name):
     conn = connect()
     c = conn.cursor()
-    c.execute("INSERT INTO meds (name, dosage, interval) VALUES (?, ?, ?)",
-              (name, dosage, interval))
+    c.execute("INSERT INTO children (name) VALUES (?)", (name,))
     conn.commit()
     conn.close()
 
-
-def get_meds():
+def get_children():
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT * FROM meds")
+    c.execute("SELECT * FROM children")
     data = c.fetchall()
     conn.close()
     return data
 
 
+# -------------------------
+# MED FUNCTIONS
+# -------------------------
+def add_med(child_id, name, dosage, interval):
+    conn = connect()
+    c = conn.cursor()
+    c.execute("""
+        INSERT INTO meds (child_id, name, dosage, interval)
+        VALUES (?, ?, ?, ?)
+    """, (child_id, name, dosage, interval))
+    conn.commit()
+    conn.close()
+
+def get_meds_by_child(child_id):
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM meds WHERE child_id=?", (child_id,))
+    data = c.fetchall()
+    conn.close()
+    return data
+
+
+# -------------------------
+# LOG FUNCTIONS
+# -------------------------
 def log_dose(med_id, time):
     conn = connect()
     c = conn.cursor()
@@ -53,7 +90,6 @@ def log_dose(med_id, time):
               (med_id, time))
     conn.commit()
     conn.close()
-
 
 def get_last_dose(med_id):
     conn = connect()
